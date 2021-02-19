1763a3447a972b3519c5fc1aba85a9a891e8c382
Ale <aadorian@gmail.com>
2021-02-19 15:35:17 -0300

    updateremove icons of moon icons of material design

--- src/containers/ConfigurationMenu/ConfigurationMenu.tsx
+++ src/containers/ConfigurationMenu/ConfigurationMenu.tsx
@@ -45,12 +45,12 @@ const PagedMenu: React.FC<IPagedMenuProps> = (props) => {
 
   return (
     <>
-       <Brightness2Icon/>
+      
       <IconButton style={{ fontSize: 14 }}onClick={(e) => props.onChange('ethereum-rpc', 'https://rpc.testnet.moonbeam.network')}>
           {t("Moonbase Alpha")}
       </IconButton>
       <Divider />
-      <Brightness3Icon/>
+    
       <IconButton style={{ fontSize: 14 }} onClick={(e) => props.onChange('ethereum-rpc', 'http://127.0.0.1:9933')}>
           {t("Standalone")}
       </IconButton>
